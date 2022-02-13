a3f141085d3899d68e1be95c73800af90163028c
NinoosMoshi <nalkaton@gmail.com>
2022-02-13 18:49:28 -0500

    rabbitMQ publisher

--- notification/src/main/java/com/ninos/notification/NotificationApplication.java
+++ notification/src/main/java/com/ninos/notification/NotificationApplication.java
@@ -1,11 +1,32 @@
 package com.ninos.notification;
 
+import com.ninos.amqp.RabbitMQMessageProducer;
+import com.ninos.notification.config.NotificationConfig;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.ninos.notification",
+                "com.ninos.amqp"
+        }
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+    @Bean
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer,
+                                        NotificationConfig notificationConfig){
+        return args -> {
+          producer.publish("foo",
+                           notificationConfig.getInternalExchange(),
+                           notificationConfig.getInternalNotificationRoutingKey());
+        };
+    }
+
+
 }
